7f2161910f96bbe4461ab780fc1b33170dbc6035
hupratt <hprattdo@gmail.com>
2020-04-05 10:21:25 +0200

    adding upscale and downscale procedures

---
+++ ec2/downsize-instance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+aws ec2 stop-instances --instance-ids i-00c1e0195543743f4
+
+sleep 40
+
+aws ec2 modify-instance-attribute --instance-id i-00c1e0195543743f4 --instance-type "{\"Value\": \"t2.micro\"}"
+
+sleep 5
+
+aws ec2 start-instances --instance-ids i-00c1e0195543743f4
+
